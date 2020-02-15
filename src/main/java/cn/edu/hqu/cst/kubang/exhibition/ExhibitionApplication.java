fefbaf0196146ee968c519f77311eaf89edace93
sunquan <2502665955@qq.com>
2020-02-15 14:53:18 +0800

    add annotation

--- src/main/java/cn/edu/hqu/cst/kubang/exhibition/ExhibitionApplication.java
+++ src/main/java/cn/edu/hqu/cst/kubang/exhibition/ExhibitionApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExhibitionApplication {
-
+    //程序入口
 	public static void main(String[] args) {
 		SpringApplication.run(ExhibitionApplication.class, args);
 	}
