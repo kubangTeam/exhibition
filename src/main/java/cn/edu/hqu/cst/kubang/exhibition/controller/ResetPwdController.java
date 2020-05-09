c2c4f730dc65fa07697453b34b058326c6797db7
SunChonggao <sunchg_mail@163.com>
2020-05-09 12:51:53 +0800

    忘记密码

---
+++ src/main/java/cn/edu/hqu/cst/kubang/exhibition/controller/ResetPwdController.java
@@ -0,0 +1,15 @@
+package cn.edu.hqu.cst.kubang.exhibition.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author SunChonggao
+ * @Date 2020/5/8 19:41
+ * @Version 1.0
+ * @Description:
+ */
+@RestController
+@Api(tags = "忘记")
+public class ResetPwdController {
+}
