c0ab1d9bb7edf14fcd6cb56ff9e4201799b6d14e
PigPigBoy <1359862182@qq.com>
2020-05-21 16:17:37 +0800

    配置maven打包时间、swagger时间

--- src/main/java/cn/edu/hqu/cst/kubang/exhibition/configuration/SwaggerConfig.java
+++ src/main/java/cn/edu/hqu/cst/kubang/exhibition/configuration/SwaggerConfig.java
@@ -10,6 +10,11 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  *  @author: 邢佳成
  *  @Date: 2020.03.04 22:34
@@ -30,8 +35,11 @@ public class SwaggerConfig {
     }
 
     private ApiInfo apiInfo() {
+        Calendar c = Calendar.getInstance(TimeZone.getTimeZone("GMT+08:00"));    //获取东八区时间
+        SimpleDateFormat s=new SimpleDateFormat("yyyy-MM-dd");
+        String curDate = s.format(c.getTime());
         return new ApiInfoBuilder()
-                .title("酷邦线上展会接口文档————2020.05.21")
+                .title("酷邦线上展会接口文档——"+ curDate + " 发布")
                 .version("v0.6")
                 .build();
     }
